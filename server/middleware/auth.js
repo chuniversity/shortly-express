e9a2880693895b95de1f047561b614c43a3fbd64
Chuniversity <contact@chunis.org>
2021-05-21 17:38:34 -0400

    begin cookie parser

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,6 +3,10 @@ const Promise = require('bluebird');
 
 // next just means "done"
 module.exports.createSession = (req, res, next) => {
+  // checking the cookie
+  // does this cookie relate to a session
+  // if they dont have a session
+  // create a session
 };
 
 /************************************************************/
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,7 +1,11 @@
 // for each post request
 const parseCookies = (req, res, next) => {
+  // define empty object
+  var cookieObj = {};
   // grab cookies from request
-  // req[cookies]
+  console.log('test1', req.headers.cookie);
+  // console.log('test2', req[cookies]);
+  req.cookies = cookieObj;
 };
 
 module.exports = parseCookies;
