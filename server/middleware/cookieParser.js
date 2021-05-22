02a5660ea6cea1ba7f3b51b559264b1de81ff0e1
psohn12 <patricksohn12@gmail.com>
2021-05-22 11:59:10 -0500

    completed createsession middleware

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,7 +1,21 @@
+
 // for each post request
 const parseCookies = (req, res, next) => {
-  // grab cookies from request
-  // req[cookies]
+  // 'shortlyid=18ea4fb6ab3178092ce936c591ddbb90c99c9f66; otherCookie=2a990382005bcc8b968f2b18f8f7ea490e990e78; anotherCookie=8a864482005bcc8b968f2b18f8f7ea490e577b20'
+  // define empty object
+  var cookieObj = {};
+  // grab cookies from request]
+  var key = 'shortlyid=';
+  if (req.headers.cookie) {
+    var theCookies = req.headers.cookie.split('; ');
+    theCookies.forEach((cookie) => {
+      var cookieSplit = cookie.split('=');
+      cookieObj[cookieSplit[0]] = cookieSplit[1];
+    });
+
+    req.cookies = cookieObj;
+  }
+  next();
 };
 
 module.exports = parseCookies;
