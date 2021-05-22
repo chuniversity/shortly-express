6b0be0ec8b494de40eb5bca085071afe8b9326d8
Chuniversity <contact@chunis.org>
2021-05-21 21:19:41 -0400

    finalizing cookies

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const utils = require('./lib/hashUtils');
 const partials = require('express-partials');
 const bodyParser = require('body-parser');
 const Auth = require('./middleware/auth');
+const cookie = require('./middleware/cookieParser');
 const models = require('./models');
+
 
 const app = express();
 
@@ -15,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // loads client to page
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookie);
 
 
 
--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,11 +1,22 @@
 // for each post request
 const parseCookies = (req, res, next) => {
+  // 'shortlyid=18ea4fb6ab3178092ce936c591ddbb90c99c9f66; otherCookie=2a990382005bcc8b968f2b18f8f7ea490e990e78; anotherCookie=8a864482005bcc8b968f2b18f8f7ea490e577b20'
   // define empty object
   var cookieObj = {};
-  // grab cookies from request
-  console.log('test1', req.headers.cookie);
-  // console.log('test2', req[cookies]);
-  req.cookies = cookieObj;
+  // grab cookies from request]
+  var key = 'shortlyid=';
+  if (req.headers.cookie) {
+    var theCookies = req.headers.cookie.split('; ');
+    theCookies.forEach((cookie) => {
+      var cookieSplit = cookie.split('=');
+      cookieObj[cookieSplit[0]] = cookieSplit[1];
+    });
+
+    req.cookies = cookieObj;
+  } else {
+
+  }
+  next();
 };
 
 module.exports = parseCookies;
